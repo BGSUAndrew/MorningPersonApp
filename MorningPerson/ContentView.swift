533847fd892a3b3a043fe944494895c86df79b90
Andrew O'Connor <andrew.oconnor.12@gmail.com>
2023-10-06 21:00:54 -0700

    Update ContentView.swift

--- MorningPerson/ContentView.swift
+++ MorningPerson/ContentView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "sunrise.fill")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.yellow)
+               
+            Text("Good morning")
         }
-        .padding()
+        Spacer()
     }
 }
 
