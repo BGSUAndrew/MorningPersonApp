c7a3130ae11eab9ed6d03d64183b35c2186476f7
Andrew O'Connor <andrew.oconnor.12@gmail.com>
2023-10-06 20:44:12 -0700

    Initial Commit

---
+++ MorningPerson/MorningPersonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorningPersonApp.swift
+//  MorningPerson
+//
+//  Created by Andrew O'Connor on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MorningPersonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
